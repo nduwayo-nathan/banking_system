1c311e1ff700fbaeaa30ae7e02eb74411a8fe7ea
Nathan NDUWAYO <nduwayonathan5@gmail.com>
2025-05-07 14:10:20 +0200

    stop tracking appplication properties

--- src/main/java/com/bank_MS/model/Customer.java
+++ src/main/java/com/bank_MS/model/Customer.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -17,8 +18,10 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String firstName;
     private String lastName;
+    @Email(message = "Invalid email format")
     private String email;
     private String mobile;
     private String dob;
